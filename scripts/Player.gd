5cb023f6a09ad7ec02bc4918740f99a97e21d424
pouiee <jonathan.jasso5@gmail.com>
2023-01-28 16:43:38 -0700

    Added Shooting

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
 var speed = 100
+var bullet_speed = 1000
+
+var bullet = preload("res://scenes/Bullet.tscn")
+
 
 func _physics_process(delta):
 	var velocity = Vector2()
@@ -18,6 +22,16 @@ func _physics_process(delta):
 		velocity = velocity.normalized() * speed
 		move_and_slide(velocity, Vector2(0, -1))
 
+
+func _process(delta):
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("fire"):
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
 
 func _ready():
 	pass
