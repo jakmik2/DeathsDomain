a0355bc575d019b5e123e2a98096a7e22f2903d8
pouiee <jonathan.jasso5@gmail.com>
2023-01-28 17:11:08 -0700

    Bullet Collisions and Enemy Explosions

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-var speed = 100
-var bullet_speed = 1000
+export var speed = 100
+export var bullet_speed = 1000
+export var fire_rate = 0.2
 
 var bullet = preload("res://scenes/Bullet.tscn")
-
+var can_fire = true
 
 func _physics_process(delta):
 	var velocity = Vector2()
@@ -26,12 +27,15 @@ func _physics_process(delta):
 func _process(delta):
 	look_at(get_global_mouse_position())
 	
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire") and can_fire:
 		var bullet_instance = bullet.instance()
-		bullet_instance.position = get_global_position()
+		bullet_instance.position = $ShootingPoint.get_global_position()
 		bullet_instance.rotation_degrees = rotation_degrees
 		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
 		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
 
 func _ready():
 	pass
