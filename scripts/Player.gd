deba9a9490383323870eaf9a394c8a8f007c07fa
pouiee <jonathan.jasso5@gmail.com>
2023-01-28 22:18:07 -0700

    Implemented Rotating Hand Gun

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,7 @@
 extends KinematicBody2D
 
 export var speed = 200
-export var bullet_speed = 500
-export var fire_rate = 0.2
 
-var bullet = preload("res://scenes/Bullet.tscn")
-var can_fire = true
 
 func _physics_process(delta):
 	var velocity = Vector2()
@@ -22,20 +18,3 @@ func _physics_process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		move_and_slide(velocity, Vector2(0, -1))
-
-
-func _process(delta):
-	look_at(get_global_mouse_position())
-	
-	if Input.is_action_pressed("fire") and can_fire:
-		var bullet_instance = bullet.instance()
-		bullet_instance.position = $ShootingPoint.get_global_position()
-		bullet_instance.rotation_degrees = rotation_degrees
-		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
-		get_tree().get_root().add_child(bullet_instance)
-		can_fire = false
-		yield(get_tree().create_timer(fire_rate), "timeout")
-		can_fire = true
-
-func _ready():
-	pass
