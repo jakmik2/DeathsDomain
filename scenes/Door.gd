4f5f542d63e0fe0ae4cc0bc1af46248a8021398d
jakmik2 <40475031+jakmik2@users.noreply.github.com>
2023-01-30 02:06:40 -0500

    Fix door disable bug

--- scenes/Door.gd
+++ scenes/Door.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var code = "0"
+var status = "closed"
 var code_from_levers = ""
 var levers = []
 
@@ -8,6 +9,7 @@ onready var anim = get_node("AnimationPlayer")
 
 func _ready():
 	levers = get_node("Levers").get_children()
+	$"StaticBody2D/CollisionPolygon2D".disabled = false
 	
 	Global.connect("lever_changed", self, "check_code")	
 
@@ -17,6 +19,12 @@ func check_code():
 		code_from_levers += lever.state
 
 	if code == code_from_levers:
-		$"StaticBody2D/CollisionPolygon2D".disabled
+		$"StaticBody2D/CollisionPolygon2D".disabled = true
 		anim.play("Open")
-		
+		status = "openned"
+	elif status == "openned":
+		anim.play("Close")
+		status = "closed"
+		$"StaticBody2D/CollisionPolygon2D".disabled = false
+	else:
+		$"StaticBody2D/CollisionPolygon2D".disabled = false
